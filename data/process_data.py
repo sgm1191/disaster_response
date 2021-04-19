7a8a4873368192503f3bc81f305ab0fb98986623
Santiago Gonzalez Medellin <santiago.gm1191@gmail.com>
2021-04-18 20:44:00 -0500

    type: feature
    data ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,51 @@
 import sys
 
+import pandas as pd
 
-def load_data(messages_filepath, categories_filepath):
-    pass
+def load_data(messages_filepath:str, categories_filepath:str):
+    """ loads disaster data from csv files
+
+    Args:
+        messages_filepath (str): path of disaster messages csv file
+        categories_filepath (str): path of disaster categories csv file
+
+    Returns:
+        pd.DataFrame: a dataframe with merged data of messages and categories
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories  = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on="id")
+    return df
 
 
 def clean_data(df):
-    pass
+    """ processes categories column and splits it into one column for each category
+    in a one-hot like way, also removes duplicates.
+
+    Args:
+        df (pd.DataFrame): dataframe of disaster messages and categories
+
+    Returns:
+        pd.DataFrame: processed data frame
+    """
+    categories = df.categories.str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = [x[:-2] for x in row]
+    categories.columns = category_colnames
+
+    for column in categories:
+        categories[column] = categories[column].astype(str).str.split('-').str.get(1)
+    categories[column] = pd.to_numeric(categories[column])
+
+    df.drop("categories", axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates()
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    with create_engine('sqlite:///tweets.db') as engine:
+        df.to_sql('disaster_messages', engine, index=False)  
 
 
 def main():
