6a7044c14a79db5e9ddbf836cdf2f18f5bd2f804
santiago gonzalez medellin <sgonzalez@na-at.com.mx>
2021-04-26 17:15:40 -0500

    type: feature
    model evaluation, build and text tokenize

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,20 +1,82 @@
+
 import sys
+
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download(['punkt', 'wordnet'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 
 
 def load_data(database_filepath):
-    pass
+    # TODO: implement database
+    df = pd.read_csv('corporate_messaging.csv', encoding='latin-1')
+    df = df[(df["category:confidence"] == 1) & (df['category'] != 'Exclude')]
+    X = df.text.values
+    y = df.category.values
+    return X, y
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('clf', RandomForestClassifier())
+    ])
+
+    parameters = {
+        'features__text_pipeline__tfidf__norm': ['l2', 'l1'],
+        'clf__n_estimators': [50, 100, 150],
+        'clf__criterion': ['gini', 'entropy']
+    }
+
+    cv = GridSearchCV(pipeline, parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test, Y_test)
+    labels = np.unique(y_pred)
+    confusion_mat = confusion_matrix(y_test, y_pred, labels=labels)
+    accuracy = (y_pred == y_test).mean()
+
+    print("Labels:", labels)
+    print("Confusion Matrix:\n", confusion_mat)
+    print("Accuracy:", accuracy)
+    print("\nBest Parameters:", model.best_params_)
 
 
 def save_model(model, model_filepath):
